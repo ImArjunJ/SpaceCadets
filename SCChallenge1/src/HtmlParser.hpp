acb5dfb55dd956113b13c9699cc198aaa759c199
ImArjunJ <arjun.juneja4u@gmail.com>
2024-10-07 21:10:15 +0100

    Migrate to HtmlParser

--- SCChallenge1/src/HtmlParser.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-namespace HtmlParser
-{
-    std::string GetHtmlFromClass(const std::string& RawHtml, const std::string& ClassName, const std::string& HtmlElement = "div");
-}
--- SCChallenge1/src/main.cpp
+++ SCChallenge1/src/main.cpp
@@ -1,9 +1,22 @@
 #include <cpr/cpr.h>
 
+#include <HtmlParser/Parser.hpp>
 #include <format>
 #include <iostream>
 
-#include "HtmlParser.hpp"
+#include "HtmlParser/DOM.hpp"
+#include "HtmlParser/Query.hpp"
+
+// Reference: https://github.com/JustCabbage/HtmlParser/blob/master/src/Utilities.hpp
+std::string Trim(const std::string& Input)
+{
+    const std::string Whitespace = " \t\n\r\f";
+    size_t Start = Input.find_first_not_of(Whitespace);
+    if (Start == std::string::npos)
+        return "";
+    size_t End = Input.find_last_not_of(Whitespace);
+    return Input.substr(Start, End - Start + 1);
+}
 
 int main(int argc, char** argv)
 {
@@ -13,8 +26,11 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    // TODO: implement input sanitization
     std::string PersonId = argv[1];
+    if (PersonId.find("@") != std::string::npos)
+    {
+        PersonId = PersonId.substr(0, PersonId.find("@"));
+    }
     cpr::Response r = cpr::Get(cpr::Url{std::format("https://www.ecs.soton.ac.uk/people/{}", PersonId)});
 
     if (r.status_code != 200)
@@ -23,13 +39,45 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    const std::string PersonName = HtmlParser::GetHtmlFromClass(r.text, "heading-m inline-block text-prussianDark", "h1");
+    HtmlParser::Parser Parser;
+    Parser.SetStrict(false); // Disable strict mode to be more forgiving
 
-    if (PersonName.empty())
+    HtmlParser::DOM Document = Parser.Parse(r.text);
+
+    if (Document.Root() == nullptr)
     {
-        std::cerr << "Person name not found" << std::endl;
+        std::cerr << "Failed to parse the page" << std::endl;
         return 1;
     }
 
-    std::cout << PersonName << std::endl;
+    HtmlParser::Query Query(Document.Root());
+
+    auto Name = Query.SelectFirst("h1.heading-m.inline-block.text-prussianDark");
+
+    if (!Name)
+    {
+        std::cerr << "This user's profile does not seem to be public, or their email does not match the id" << std::endl;
+        return 1;
+    }
+
+    auto TitleNode = Query.SelectFirst("div.pb-6.text-xl");
+    auto AboutNode = Query.SelectFirst("section.sidetabs-section.mb-25");
+    auto TabBar = Query.SelectFirst("div[role='tablist']");
+
+    std::string Title = TitleNode == nullptr ? "Not Found" : TitleNode->GetTextContent();
+    std::string About = AboutNode == nullptr ? "Not Found" : AboutNode->GetTextContent();
+
+    std::cout << "  Name: " << Name->GetTextContent() << "\n";
+    std::cout << "  Title: " << Title << "\n";
+    std::cout << "  About:\n";
+
+    std::istringstream AboutStream(Trim(About));
+    std::string Line;
+
+    while (std::getline(AboutStream, Line))
+    {
+        std::cout << "    " << Line << "\n";
+    }
+
+    return 0;
 }
